fc720575299c37007c538bf180d199ddf3111ab9
rey <reymusic111@gmail.com>
2022-12-31 15:24:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
     InputTextMessageContent
-from plugins.forcesub import ForceSub
 import asyncio
 
 # Bot Client for Inline Search
