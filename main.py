3a0139c8a2e3887b4f01f8e5d30100f00df1a612
rey <reymusic111@gmail.com>
2022-12-31 13:12:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
     InputTextMessageContent
-from teamteleroid.forcesub import ForceSub
+from plugins.forcesub import ForceSub
 import asyncio
 
 # Bot Client for Inline Search
